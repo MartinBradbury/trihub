385f33153fe79413557a3d6a4a1c733ee4b7248d
Martin Bradbury <martinbradbury@btinternet.com>
2024-06-15 19:15:57 +0000

    created axios interceptors to refrest user tokens and routing if they cannot be refreshed

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://trihub-api-0d6dd53f4e6b.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
