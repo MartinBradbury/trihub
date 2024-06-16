9174f5e14bbf05bcf1bb8e878d4f9c92136e5805
Martin Bradbury <martinbradbury@btinternet.com>
2024-06-16 12:39:05 +0000

    added conditional rendering to only display background video on "/" homepage.

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import styles from "../styles/Video.module.css";
 import video from "../assets/videoplayback.mp4";
+import { useLocation } from 'react-router-dom';
 
 const HomePage = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
+  if (!isHomePage) {
+    return null;
+  }
+
   return (
     <div className={styles.Background}>
       <video autoPlay muted loop className={styles.Video}>
@@ -11,13 +19,10 @@ const HomePage = () => {
       </video>
       <p>Test</p>
       <div className={styles.Content}>
-        <h1>
-            Welcome to my website
-        </h1>
-
+        <h1>Welcome to my website</h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default HomePage
