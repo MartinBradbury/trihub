59cec5639fc3f08af219215cbb285fc90ef3b09f
Martin Bradbury <martinbradbury@btinternet.com>
2024-06-26 15:05:26 +0000

    added initial styling to homepage

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 import styles from "../styles/Video.module.css";
 import video from "../assets/videoplayback.mp4";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
+import logo from "../assets/trihubnobg.png";
 
 const HomePage = () => {
   const location = useLocation();
-  const isHomePage = location.pathname === '/';
+  const isHomePage = location.pathname === "/";
 
   if (!isHomePage) {
     return null;
@@ -13,16 +14,18 @@ const HomePage = () => {
 
   return (
     <div className={styles.Background}>
-      <video autoPlay muted loop className={styles.Video}>
+      <video autoPlay muted loop className={styles.Video} alt="triathlon background video">
         <source src={video} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <p>Test</p>
       <div className={styles.Content}>
-        <h1>Welcome to my website</h1>
+        <img src={logo} alt="trihub logo"/>
+        <h3>A Triathlon Community</h3>
+        <p>For triathletes of all abilities</p>
       </div>
     </div>
   );
 };
 
-export default HomePage
+export default HomePage;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import styles from "../../styles/Home.module.css";
+import bgimg from "../../assets/signupimg.jpg";
 
 const Home = () => {
   return (
-    <div>this is a test</div>
-  )
-}
+    <>
+      <Container>
+        <div>
+          <h1>WELCOME TO THE WEBSITE HERE YOU WILL FIND...</h1>
+        </div>
 
-export default Home
+        <Row>
+          <Col>
+            <Card className={styles.card} md={6} lg={12}>
+              <Card.Img src={bgimg} className={styles.img} />
+              <Card.Body className={styles.overlay}>
+                <Card.Title><h1>British Triathlon Website</h1></Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Card className={styles.card} md={6} lg={12}>
+              <Card.Img src={bgimg} className="card-img-top" />
+              <Card.Body className={styles.overlay}>
+                <Card.Title><h3>British Triathlon Website</h3></Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card className={styles.card} md={6} lg={12}>
+              <Card.Img src={bgimg} className="card-img-top" />
+              <Card.Body className={styles.overlay}>
+                <Card.Title><h3>British Triathlon Website</h3></Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Home;
